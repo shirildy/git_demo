a8abf2e0a5e1431fd3c402f1f6a0ae24ba5d86f6
shiril <shiril.dy@gmail.com>
2025-06-29 10:49:30 +1000

    git_demo application

---
+++ git_demo_app1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# this is very simple shell script to print the hello message 
+## Usage: ./hello.sh [name]
+
+
+if [ -z "$1" ]; then
+  echo "Hello, World!"
+else
+  echo "Hello, $1!"
+fi
+
